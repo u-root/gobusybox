f0f493c2348d5409e511eeb39d234beb45884ebd
Chris Koch <chrisko@google.com>
2020-09-05 07:02:27 -0700

    ok this actually works
    Signed-off-by: Chris Koch <chrisko@google.com>

--- test.sh
+++ test.sh
@@ -28,21 +28,23 @@ function ctrl_c() {
 }
 trap ctrl_c INT
 
+# u-root checked out NOT in $GOPATH.
 (cd $TMPDIR && git clone https://github.com/u-root/u-root)
 
 # Make u-root have modules.
 (cd $TMPDIR/u-root && go mod init github.com/u-root/u-root && go mod vendor)
-GO111MODULE=auto ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
-
 # Disarm vendor directory.
 (cd $TMPDIR/u-root && mv vendor vendor2)
+
+GO111MODULE=auto ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
 GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
 
-# This should work as is, too.
-GO111MODULE=on ./src/cmd/makebb/makebb github.com/u-root/u-root/cmds/...
+# This should work as is, too. It'll pull it straight from the internet.
+#GO111MODULE=on ./src/cmd/makebb/makebb github.com/u-root/u-root/cmds/...
 rm -rf $TMPDIR
 
 
+# Try vendor-based $GOPATH u-root.
 GOPATH_TMPDIR=$(mktemp -d)
 function ctrl_c() {
   rm -rf $GOPATH_TMPDIR
