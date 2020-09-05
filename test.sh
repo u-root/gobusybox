44e53d0bcfb4d605dab3b1688dd5af72334507a7
Chris Koch <chrisko@google.com>
2020-09-04 23:44:32 -0700

    Add tools necessary for bazel build systems
    Signed-off-by: Chris Koch <chrisko@google.com>

--- test.sh
+++ test.sh
@@ -16,3 +16,38 @@ cd ../../..
 GO111MODULE=on ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace
 GO111MODULE=auto ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace
 GO111MODULE=off ./src/cmd/makebb/makebb vendortest/cmd/dmesg vendortest/cmd/strace
+
+TMPDIR=$(mktemp -d)
+
+function ctrl_c() {
+  rm -rf $TMPDIR
+}
+trap ctrl_c INT
+
+(cd $TMPDIR && git clone https://github.com/u-root/u-root)
+
+# Make u-root have modules.
+(cd $TMPDIR/u-root && go mod init github.com/u-root/u-root && go mod vendor)
+GO111MODULE=auto ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
+
+# Disarm vendor directory.
+(cd $TMPDIR/u-root && mv vendor vendor2)
+GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
+
+# This should work as is, too.
+GO111MODULE=on ./src/cmd/makebb/makebb github.com/u-root/u-root/cmds/...
+rm -rf $TMPDIR
+
+
+GOPATH_TMPDIR=$(mktemp -d)
+function ctrl_c() {
+  rm -rf $GOPATH_TMPDIR
+}
+trap ctrl_c INT
+
+(cd $GOPATH_TMPDIR && GOPATH=$GOPATH_TMPDIR GO111MODULE=off go get -u github.com/u-root/u-root)
+GOPATH=$GOPATH_TMPDIR GO111MODULE=off ./src/cmd/makebb/makebb github.com/u-root/u-root/cmds/...
+rm -rf $GOPATH_TMPDIR
+
+# try bazel build
+(cd src && bazel build :model_bb)
