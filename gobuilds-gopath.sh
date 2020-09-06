5f2ddd2d762d509930f8077ed716bcedb6d625ea
Chris Koch <chrisko@google.com>
2020-09-05 19:17:31 -0700

    tests that work in $GOPATH and outside $GOPATH
    Signed-off-by: Chris Koch <chrisko@google.com>

---
+++ gobuilds-gopath.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eux
+
+# all the go module builds should still work in $GOPATH
+./gobuilds.sh
+
+cd src/cmd/makebb
+
+# This uses the go.mod in src/
+GO111MODULE=off ./makebb ../../../vendortest/cmd/dmesg ../../../vendortest/cmd/strace
+
+# mix vendor-based cmds with mod-based cmds. only works with GO111MODULE=off iff
+# dependencies are in $GOPATH.
+GO111MODULE=off ./makebb ../../../vendortest/cmd/dmesg ../../../modtest/cmd/strace
+GO111MODULE=off ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace
+GO111MODULE=off ./makebb ../../../modtest/cmd/dmesg ../../../vendortest/cmd/strace
+
+# Make sure `makebb` works completely out of its own tree: there is no go.mod at
+# the top of the tree that `go` can fall back on.
+cd ../../..
+GO111MODULE=off ./src/cmd/makebb/makebb vendortest/cmd/dmesg vendortest/cmd/strace
+
+GO111MODULE=off ./src/cmd/makebb/makebb vendortest/cmd/dmesg modtest/cmd/strace
+GO111MODULE=off ./src/cmd/makebb/makebb modtest/cmd/dmesg vendortest/cmd/strace
+GO111MODULE=off ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace
