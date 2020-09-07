33645cc3fd847338df3d7840666cb1a02cf2107a
Chris Koch <chrisko@google.com>
2020-09-07 03:15:14 -0700

    test: add a dependency with a version number
    Signed-off-by: Chris Koch <chrisko@google.com>

--- test/mod1/cmd/hellowithdep/hello.go
+++ test/mod1/cmd/hellowithdep/hello.go
@@ -7,10 +7,14 @@ import (
 	"github.com/u-root/gobusybox/test/mod1/pkg/hello"
 	"github.com/u-root/gobusybox/test/mod2/pkg/exthello"
 	hello2 "github.com/u-root/gobusybox/test/mod2/pkg/hello"
+
+	// A package whose import path does not match its $GOPATH.
+	hello4 "github.com/u-root/gobusybox/test/mod4/v2/pkg/hello"
 )
 
 func main() {
 	fmt.Printf("test/mod1/hello: %s\n", hello.Hello())
 	fmt.Printf("test/mod2/hello: %s\n", hello2.Hello())
 	fmt.Printf("test/mod2/exthello: %s\n", exthello.Hello())
+	fmt.Printf("test/mod4/hello: %s\n", hello4.Hello())
 }
