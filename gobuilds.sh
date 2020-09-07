613c7fc7a810f5dae02ab4b28c3a599d1a5c99fe
Chris Koch <chrisko@google.com>
2020-09-07 02:52:26 -0700

    Actually test that busybox invocation works by invoking some commands.
    also, getpid -> getppid so we have something to test against.
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- gobuilds.sh
+++ gobuilds.sh
@@ -4,6 +4,15 @@ set -eux
 cd src/cmd/makebb
 go build
 
+function testmod1() {
+  BPID=$(echo $$);
+  GETPPID=$(./bb getppid);
+  test "$BPID" == "$GETPPID" || (echo "PIDs not the same" && exit 1)
+
+  HW=$(./bb helloworld);
+  test "$HW" == "hello world" || (echo "hello world not right" && exit 1)
+}
+
 # This uses the go.mod in src/
 GO111MODULE=on ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace
 test -f ./bb || exit 1
@@ -13,12 +22,14 @@ GO111MODULE=auto ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace
 test -f ./bb || exit 1
 rm ./bb
 
-GO111MODULE=on ./makebb ../../../test/mod1/cmd/helloworld ../../../test/mod1/cmd/getpid ../../../test/mod1/cmd/hellowithdep
+GO111MODULE=on ./makebb ../../../test/mod1/cmd/helloworld ../../../test/mod1/cmd/getppid ../../../test/mod1/cmd/hellowithdep
 test -f ./bb || exit 1
+testmod1
 rm ./bb
 
-GO111MODULE=auto ./makebb ../../../test/mod1/cmd/helloworld ../../../test/mod1/cmd/getpid ../../../test/mod1/cmd/hellowithdep
+GO111MODULE=auto ./makebb ../../../test/mod1/cmd/helloworld ../../../test/mod1/cmd/getppid ../../../test/mod1/cmd/hellowithdep
 test -f ./bb || exit 1
+testmod1
 rm ./bb
 
 # nested modules
@@ -49,10 +60,12 @@ GO111MODULE=auto ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace mo
 test -f ./bb || exit 1
 rm ./bb
 
-GO111MODULE=on ./src/cmd/makebb/makebb test/mod1/cmd/helloworld test/mod1/cmd/getpid test/mod1/cmd/hellowithdep
+GO111MODULE=on ./src/cmd/makebb/makebb test/mod1/cmd/helloworld test/mod1/cmd/getppid test/mod1/cmd/hellowithdep
 test -f ./bb || exit 1
+testmod1
 rm ./bb
 
-GO111MODULE=auto ./src/cmd/makebb/makebb test/mod1/cmd/helloworld test/mod1/cmd/getpid test/mod1/cmd/hellowithdep
+GO111MODULE=auto ./src/cmd/makebb/makebb test/mod1/cmd/helloworld test/mod1/cmd/getppid test/mod1/cmd/hellowithdep
 test -f ./bb || exit 1
+testmod1
 rm ./bb
