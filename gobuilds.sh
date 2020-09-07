c96929b5342be516b27d94228f75139978c60dcc
Chris Koch <chrisko@google.com>
2020-09-06 23:51:21 -0700

    bb: fix nested modules loading
    Signed-off-by: Chris Koch <chrisko@google.com>

--- gobuilds.sh
+++ gobuilds.sh
@@ -9,8 +9,8 @@ GO111MODULE=on ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace
 GO111MODULE=auto ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace
 
 # nested modules
-#GO111MODULE=on ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace ../../../modtest/nestedmod/p9ufs
-#GO111MODULE=auto ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace ../../../modtest/nestedmod/p9ufs
+GO111MODULE=on ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace ../../../modtest/nestedmod/p9ufs
+GO111MODULE=auto ./makebb ../../../modtest/cmd/dmesg ../../../modtest/cmd/strace ../../../modtest/nestedmod/p9ufs
 
 # Make sure `makebb` works completely out of its own tree: there is no go.mod at
 # the top of the tree that `go` can fall back on.
@@ -18,5 +18,5 @@ cd ../../..
 GO111MODULE=on ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace
 GO111MODULE=auto ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace
 
-#GO111MODULE=on ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace modtest/nestedmod/p9ufs
-#GO111MODULE=auto ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace modtest/nestedmod/p9ufs
+GO111MODULE=on ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace modtest/nestedmod/p9ufs
+GO111MODULE=auto ./src/cmd/makebb/makebb modtest/cmd/dmesg modtest/cmd/strace modtest/nestedmod/p9ufs
--- src/pkg/bb/bb_test.go
+++ src/pkg/bb/bb_test.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/u-root/gobusybox/src/pkg/golang"
@@ -30,5 +31,62 @@ func DISABLEDTestPackageRewriteFile(t *testing.T) {
 	o, err := cmd.CombinedOutput()
 	if err != nil {
 		t.Fatalf("foo failed: %v %v", string(o), err)
+	}
+}
+
+func TestModules(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test-modules-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	os.MkdirAll(filepath.Join(dir, "mod1/cmd/cmd1"), 0755)
+	os.MkdirAll(filepath.Join(dir, "mod1/cmd/cmd2"), 0755)
+	os.MkdirAll(filepath.Join(dir, "mod1/nestedmod1/cmd/cmd5"), 0755)
+	os.MkdirAll(filepath.Join(dir, "mod1/nestedmod2/cmd/cmd6"), 0755)
+	os.MkdirAll(filepath.Join(dir, "mod2/cmd/cmd3"), 0755)
+	os.MkdirAll(filepath.Join(dir, "mod2/cmd/cmd4"), 0755)
+	os.MkdirAll(filepath.Join(dir, "nomod/cmd/cmd7"), 0755)
+	ioutil.WriteFile(filepath.Join(dir, "mod1/go.mod"), nil, 0644)
+	ioutil.WriteFile(filepath.Join(dir, "mod1/nestedmod1/go.mod"), nil, 0644)
+	ioutil.WriteFile(filepath.Join(dir, "mod1/nestedmod2/go.mod"), nil, 0644)
+	ioutil.WriteFile(filepath.Join(dir, "mod2/go.mod"), nil, 0644)
+
+	paths := []string{
+		filepath.Join(dir, "mod1/cmd/cmd1"),
+		filepath.Join(dir, "mod1/cmd/cmd2"),
+		filepath.Join(dir, "mod1/nestedmod1/cmd/cmd5"),
+		filepath.Join(dir, "mod1/nestedmod2/cmd/cmd6"),
+		filepath.Join(dir, "mod2/cmd/cmd3"),
+		filepath.Join(dir, "mod2/cmd/cmd4"),
+		filepath.Join(dir, "nomod/cmd/cmd7"),
+	}
+	mods, noModulePkgs := modules(paths)
+
+	want := map[string][]string{
+		filepath.Join(dir, "mod1"): {
+			filepath.Join(dir, "mod1/cmd/cmd1"),
+			filepath.Join(dir, "mod1/cmd/cmd2"),
+		},
+		filepath.Join(dir, "mod1/nestedmod1"): {
+			filepath.Join(dir, "mod1/nestedmod1/cmd/cmd5"),
+		},
+		filepath.Join(dir, "mod1/nestedmod2"): {
+			filepath.Join(dir, "mod1/nestedmod2/cmd/cmd6"),
+		},
+		filepath.Join(dir, "mod2"): {
+			filepath.Join(dir, "mod2/cmd/cmd3"),
+			filepath.Join(dir, "mod2/cmd/cmd4"),
+		},
+	}
+	if !reflect.DeepEqual(mods, want) {
+		t.Errorf("modules() = %v, want %v", mods, want)
+	}
+	wantNoModule := []string{
+		filepath.Join(dir, "nomod/cmd/cmd7"),
+	}
+	if !reflect.DeepEqual(noModulePkgs, wantNoModule) {
+		t.Errorf("modules() no module pkgs = %v, want %v", noModulePkgs, wantNoModule)
 	}
 }
