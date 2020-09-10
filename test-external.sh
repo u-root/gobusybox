ff6889fe9c3b142eeed7ceda1e98ff94fafe1f70
Chris Koch <chrisko@google.com>
2020-09-09 23:35:38 -0700

    circleci: add builds of u-root, u-bmc, gokrazy, p9
    Signed-off-by: Chris Koch <chrisko@google.com>

--- test-external.sh
+++ test-external.sh
@@ -18,15 +18,22 @@ trap ctrl_c INT
 # u-root checked out NOT in $GOPATH.
 (cd $TMPDIR && git clone https://github.com/u-root/u-root)
 (cd $TMPDIR && git clone https://github.com/gokrazy/gokrazy)
+(cd $TMPDIR && git clone https://github.com/u-root/u-bmc)
+(cd $TMPDIR && git clone https://github.com/hugelgupf/p9)
 
 # Make u-root have modules.
-(cd $TMPDIR/u-root && go mod init github.com/u-root/u-root && go mod vendor)
-# Disarm vendor directory.
-(cd $TMPDIR/u-root && mv vendor vendor2)
+(cd $TMPDIR/u-root && go mod init github.com/u-root/u-root && rm -rf vendor)
+
+# Make u-bmc have modules, and use local u-root.
+(cd $TMPDIR/u-bmc && go mod init github.com/u-root/u-bmc)
+echo "replace github.com/u-root/u-root => ../u-root" >> $TMPDIR/u-bmc/go.mod
+
+# Make p9 use local u-root.
+echo "replace github.com/u-root/u-root => ../u-root" >> $TMPDIR/p9/go.mod
 
 GO111MODULE=auto ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
 GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/*
-GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/* $TMPDIR/gokrazy/cmd/*
+GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/* $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/* $TMPDIR/u-bmc/cmd/*
 
 # This should work as is, too. It'll pull it straight from the internet.
 #GO111MODULE=on ./src/cmd/makebb/makebb github.com/u-root/u-root/cmds/...
