34cb7b54ccb1a32f59ec70dce7b0aea1fd4e8e39
Chris Koch <chrisko@google.com>
2020-09-11 18:40:31 -0700

    monoimporter: fix conflicting stdlib package names
    Signed-off-by: Chris Koch <chrisko@google.com>

---
+++ src/bazeltest/cmd/stdlibconflict/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+
+	// /math.a conflicts with /runtime/internal/math.a sometimes. Make sure
+	// that doesn't happen.
+	"math"
+)
+
+func main() {
+	log.Printf("MaxInt64: %d", math.MaxInt64)
+}
