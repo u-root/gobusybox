254c5808cb393eeb2b263d2d15dbc33371312836
Chris Koch <chrisko@google.com>
2021-05-28 07:00:18 -0700

    test: remove u-bmc -- it's hard to make it go-mod-compatible on the fly
    test-external.sh attempts to generate a good go.mod and go.sum for u-bmc
    on the fly. As of Go 1.16 that's hard.
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- test-external.sh
+++ test-external.sh
@@ -26,24 +26,12 @@ function ctrl_c() {
 trap ctrl_c INT
 
 # u-root checked out NOT in $GOPATH.
-(cd $TMPDIR && git clone https://github.com/u-root/u-root)
+(cd $TMPDIR && git clone --branch circ-dep https://github.com/hugelgupf/u-root)
 (cd $TMPDIR && git clone https://github.com/gokrazy/gokrazy)
-(cd $TMPDIR && git clone https://github.com/u-root/u-bmc)
 (cd $TMPDIR && git clone https://github.com/hugelgupf/p9)
 
 # Make u-root have modules.
 (cd $TMPDIR/u-root && rm -rf vendor)
-
-# Make u-bmc have modules, and use local u-root.
-(cd $TMPDIR/u-bmc && $GO mod init github.com/u-root/u-bmc)
-(cd $TMPDIR/u-bmc && touch config/i_agree_to_the_acme_terms)
-# fake ssh key, whatever.
-(cd $TMPDIR/u-bmc && touch ssh_keys.pub)
-(cd $TMPDIR/u-bmc && $GO generate ./config)
-echo "replace github.com/u-root/u-root => ../u-root" >> $TMPDIR/u-bmc/go.mod
-
-# Make p9 use local u-root.
-echo "replace github.com/u-root/u-root => ../u-root" >> $TMPDIR/p9/go.mod
 
 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=auto ./src/cmd/makebb/makebb -o bb1 $TMPDIR/u-root/cmds/*/*
 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb -o bb2 $TMPDIR/u-root/cmds/*/*
@@ -53,7 +41,6 @@ rm bb1 bb2
 
 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/* $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/*
 GOARCH=arm64 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/* $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/*
-GOARM=5 GOARCH=arm GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/core/* $TMPDIR/u-bmc/cmd/* $TMPDIR/u-bmc/platform/quanta-f06-leopard-ddr3/cmd/*
 
 if grep -q -v "go1.13" <<< "$($GO version)"; then
   GOARCH=riscv64 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/u-root/cmds/*/* $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/*
