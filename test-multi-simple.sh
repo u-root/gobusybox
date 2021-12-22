697c954999a31022021c7d36b416162ebbf72406
Chris Koch <chrisko@google.com>
2021-12-22 02:11:03 -0800

    test: only try compiling u-root after Go 1.17
    Signed-off-by: Chris Koch <chrisko@google.com>

---
+++ test-multi-simple.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -ex
+
+if [ -z "$GOROOT" ]; then
+  GO="go"
+else
+  GO="$GOROOT/bin/go"
+fi
+
+cd src/cmd/makebb
+
+$GO generate
+$GO build
+
+cd ../../..
+
+TMPDIR=$(mktemp -d)
+EMPTY_TMPDIR=$(mktemp -d)
+
+function ctrl_c() {
+  rm -rf $TMPDIR
+  # https://github.com/golang/go/issues/27455
+  GOPATH=$EMPTY_TMPDIR $GO clean -cache -modcache
+  rm -rf $EMPTY_TMPDIR
+}
+trap ctrl_c INT
+
+(cd $TMPDIR && git clone https://github.com/gokrazy/gokrazy)
+(cd $TMPDIR && git clone https://github.com/hugelgupf/p9)
+
+# Compile gokrazy and p9 together. Got ideas for what to add here? Let me know.
+GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/*
+GOARCH=arm64 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/*
+
+if grep -q -v "go1.13" <<< "$($GO version)"; then
+  GOARCH=riscv64 GOROOT=$GOROOT GOPATH=$EMPTY_TMPDIR GO111MODULE=on ./src/cmd/makebb/makebb $TMPDIR/gokrazy/cmd/* $TMPDIR/p9/cmd/*
+fi
+
+rm -rf $TMPDIR
