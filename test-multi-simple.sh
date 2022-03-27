9a3a32e990620e09a99dab11919300153958eadf
Chris Koch <chrisko@google.com>
2022-03-27 16:06:35 -0700

    Pin gokrazy commit
    gokrazy does not have a valid go.sum file according to Go 1.16, so we
    gotta pin it to before it got messed up.
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- test-multi-simple.sh
+++ test-multi-simple.sh
@@ -25,7 +25,7 @@ function ctrl_c() {
 }
 trap ctrl_c INT
 
-(cd $TMPDIR && git clone https://github.com/gokrazy/gokrazy)
+(cd $TMPDIR && git clone https://github.com/gokrazy/gokrazy && cd gokrazy && git checkout 1c3bb2365452542d52263bd4eeaafec0b83e90e2)
 (cd $TMPDIR && git clone https://github.com/hugelgupf/p9)
 
 # Compile gokrazy and p9 together. Got ideas for what to add here? Let me know.
