0e23eda1566797142e6711c5751642d0bcc5a3ee
Chris Koch <chrisko@google.com>
2022-12-28 16:40:02 -0800

    Run findpkg tests on presubmit
    Signed-off-by: Chris Koch <chrisko@google.com>

--- gobuilds-gopath.sh
+++ gobuilds-gopath.sh
@@ -9,3 +9,16 @@ GO111MODULE=off ./makebb ../../../vendortest/cmd/*
 
 cd ../../..
 GO111MODULE=off ./src/cmd/makebb/makebb vendortest/cmd/*
+
+TMPDIR=$(mktemp -d)
+function ctrl_c() {
+  rm -rf $TMPDIR
+  # https://github.com/golang/go/issues/27455
+  GOPATH=$EMPTY_TMPDIR $GO clean -cache -modcache
+  rm -rf $EMPTY_TMPDIR
+}
+trap ctrl_c INT
+
+(cd $TMPDIR && git clone https://github.com/u-root/u-root)
+(cd ./src && GO111MODULE=on go test -cover ./pkg/bb/findpkg/... --uroot-source=$TMPDIR/u-root)
+rm -rf $TMPDIR
