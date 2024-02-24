187ce0585db070d8ff26416c059b0d532df977b6
Chris Koch <chrisko@google.com>
2024-02-24 22:24:09 +0000

    Remove uroot source requirement on findpkg test
    Signed-off-by: Chris Koch <chrisko@google.com>

--- gotest.sh
+++ gotest.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 set -eux
 
-TMPDIR=$(mktemp -d)
-function ctrl_c() {
-  rm -rf $TMPDIR
-  # https://github.com/golang/go/issues/27455
-  GOPATH=$EMPTY_TMPDIR $GO clean -cache -modcache
-  rm -rf $EMPTY_TMPDIR
-}
-trap ctrl_c INT
-
-# Checkout before 1.20+ was required.
-(cd $TMPDIR && git clone https://github.com/u-root/u-root && cd u-root && git checkout 6ca118b0a77c23ae859cddeee15762d9cd74c63f)
-(cd ./src && GO111MODULE=on go test -cover ./pkg/bb/findpkg/... --uroot-source=$TMPDIR/u-root)
-rm -rf $TMPDIR
+(cd ./src && GO111MODULE=on go test -cover ./pkg/bb/findpkg/...)
