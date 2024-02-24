720f7694b5d147682b2bd29c7c6ffb34869c0927
Chris Koch <chrisko@google.com>
2024-02-24 23:11:44 +0000

    Convert GOPATH tests to GH
    Signed-off-by: Chris Koch <chrisko@google.com>

--- gobuilds.sh
+++ gobuilds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eux
 
-(cd ./src/cmd/makebb && GO111MODULE=on go build .)
+(cd ./src/cmd/makebb && GO111MODULE=on go build -covermode=atomic .)
 (cd ./test && go test --makebb=../src/cmd/makebb/makebb -v)
