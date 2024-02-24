3bd47659f9a7198b39ec3e15cfb3a3a960e8f257
Chris Koch <chrisko@google.com>
2024-02-24 19:52:07 +0000

    bb: always build with GO111MODULE=off
    Now that findpkg has dropped support for finding packages that are not
    also found by `go list`, we can simplify busybox module resolution as
    well.
    
    To support workspaces, modules, and no-module compilation, we query `go
    list` for all dependencies, write them to the temporary directory, and
    build with `GO111MODULE=off`. This means we will always support offline
    compilation (E.g. a vendored workspace or a vendored module will remain
    offline.)
    
    Signed-off-by: Chris Koch <chrisko@google.com>

--- gobuilds.sh
+++ gobuilds.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -eux
 
-# This one hasn't been migrated to the go test yet.
-(cd ./test/requestconflict && ./test.sh)
-
 (cd ./src/cmd/makebb && GO111MODULE=on go build .)
 (cd ./test && go test --makebb=../src/cmd/makebb/makebb -v)
